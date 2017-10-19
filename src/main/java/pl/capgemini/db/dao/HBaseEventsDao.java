ab89c8b52b03f5f3edd348c2c8b44a34b252c6c7
Michał Basiński <michal.basinski88@gmail.com>
2017-10-19 15:28:23 +0200

    Dao stub

--- src/main/java/pl/capgemini/db/dao/HBaseEventsDao.java
+++ src/main/java/pl/capgemini/db/dao/HBaseEventsDao.java
@@ -6,6 +6,7 @@ import pl.capgemini.db.entities.Event;
 @Component
 public class HBaseEventsDao {
     public void save(Event event) {
+        System.out.println(event.getDescription());
         //TODO: implement saving object
     }
 }
--- src/main/java/pl/capgemini/services/MessageService.java
+++ src/main/java/pl/capgemini/services/MessageService.java
@@ -3,6 +3,7 @@ package pl.capgemini.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.capgemini.db.dao.HBaseEventsDao;
+import pl.capgemini.db.entities.Event;
 
 @Component
 public class MessageService {
@@ -11,6 +12,8 @@ public class MessageService {
     private HBaseEventsDao eventsDao;
 
     public void saveEvent(String message) {
-        System.out.println(message);
+        Event event = new Event();
+        event.setDescription(message);
+        eventsDao.save(event);
     }
 }
